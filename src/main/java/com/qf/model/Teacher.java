eb88808f610e9304dc624a398360ecc23f565438
fhc941225 <fhc941225@163.com>
2018-10-18 10:32:11 +0800

    上传1.0

--- src/main/java/com/qf/model/Teacher.java
+++ src/main/java/com/qf/model/Teacher.java
@@ -5,9 +5,9 @@ public class Teacher {
     private Integer tea_number;
     private String tea_name;
     private String tea_birthday;
-    private Integer tea_card;
+    private String tea_card;
     private String tea_address;
-    private Integer tea_phone;
+    private String tea_phone;
     private String tea_courses;
     private String tea_pwd;
     private Integer tea_state;
@@ -44,11 +44,11 @@ public class Teacher {
         this.tea_birthday = tea_birthday;
     }
 
-    public Integer getTea_card() {
+    public String getTea_card() {
         return tea_card;
     }
 
-    public void setTea_card(Integer tea_card) {
+    public void setTea_card(String tea_card) {
         this.tea_card = tea_card;
     }
 
@@ -60,11 +60,11 @@ public class Teacher {
         this.tea_address = tea_address;
     }
 
-    public Integer getTea_phone() {
+    public String getTea_phone() {
         return tea_phone;
     }
 
-    public void setTea_phone(Integer tea_phone) {
+    public void setTea_phone(String tea_phone) {
         this.tea_phone = tea_phone;
     }
 

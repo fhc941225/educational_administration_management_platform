0fe5cc7761d7a8108a80e1190982a191c1d41aae
Assassin-EZIO <1141988108@qq.com>
2018-10-19 10:18:29 +0800

    v 1.1

---
+++ src/main/java/com/qf/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.qf.interceptor;
+
+import com.qf.model.Student;
+import com.qf.model.Teacher;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        Student student = (Student)session.getAttribute("student");
+        Teacher teacher = (Teacher)session.getAttribute("teacher");
+        if(student == null && teacher == null){
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath()+"/index");
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
